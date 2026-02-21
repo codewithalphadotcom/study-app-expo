2ef71e1769c0b79e11f7d5c147e9b38f3d1f23b2
burakorkmez <burakorkmezz@gmail.com>
2026-02-21 23:49:42 +0800

    Add ChannelScreen and ThreadScreen components with navigation and layout setup; implement EmptyState component for message lists

---
+++ src/app/channel/[cid]/thread/[cid]/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+const ThreadScreen = () => {
+  return (
+    <View>
+      <Text>ThreadScreen</Text>
+    </View>
+  );
+};
+export default ThreadScreen;
